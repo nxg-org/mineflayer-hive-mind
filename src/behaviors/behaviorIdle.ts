0369538e35eea17cc017ee841448d6909f579d22
GenerelSchwerz <xXSG.KingXx@gmail.com>
2021-12-31 15:14:07 -0500

    fixed minor things

--- src/behaviors/behaviorIdle.ts
+++ src/behaviors/behaviorIdle.ts
@@ -4,18 +4,13 @@ import { HiveBehavior } from '../HiveMindStates'
 /**
  * The bot will stand idle and do... nothing.
  */
-export class BehaviorIdle implements HiveBehavior {
+export class BehaviorIdle extends HiveBehavior {
   stateName: string = 'idle'
   active: boolean = false
   autonomous: boolean = false
   bots: Bot[] = []
 
-  constructor() {
-
-  }
-
-
-  onStateEntered(...bots: Bot[]) {
-      this.bots = bots
+  constructor(bot: Bot) {
+    super(bot)
   }
 }
